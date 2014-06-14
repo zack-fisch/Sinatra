c997b3c1526a7fad40b6b4de2f33c2c271fcdcef
Zack Fisch <zack@zfisch.com>
2014-06-14 11:51:40 -0400

    external routes with slim implemented

--- main.rb
+++ main.rb
@@ -1,28 +1,11 @@
 require 'sinatra'
 require 'slim'
-
+ 
 get '/' do
-	slim :index
+  slim :index
 end
 
-__END__ # In slim templating, all views are in the .rb file and come after __END__
-# layout is also occurs in same file
-
-
-@@layout
-doctype html
-html
-	head
-		meta charset="utf-8"
-		title Just Do it
-		link rel ="stylesheet" media="screen, projection" href="/styles.css"
-		/if[lt IE 9]
-				sscript src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
-		body
-			h1 Just Do it
-			== yield
-
-@@index 
-h2 my Tasks
-ul.Tasks
-	li Get Milk
+get '/:task' do
+	@task = params[:task].split('-').join(' ').capitalize
+	slim :task
+end
